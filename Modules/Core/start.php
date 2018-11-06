4d565035f42547e584e6afa2235d8b5806758f7c
Sarfraz Ahmed <sarfraz@onsupport.com>
2018-11-06 15:41:56 +0500

    update

--- Modules/Core/start.php
+++ Modules/Core/start.php
@@ -4,3 +4,8 @@
 foreach (glob(__DIR__ . '/Helpers/*.php') as $filename) {
     require_once($filename);
 }
+
+// load directives
+foreach (glob(base_path(__DIR__ . '/Resources/views/directives/*.php')) as $filename) {
+    require_once($filename);
+}
