9e4660c685e1dfe931a2af43741ea347c74bb274
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2018-12-07 19:31:42 +0500

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,7 +9,16 @@ Vue.use(VueRouter);
 /////////////////////////////////////////////////////////////////////////////
 // VueJS Components
 /////////////////////////////////////////////////////////////////////////////
-Vue.component('tasks-component', require('./components/task/view.vue'));
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+const files = require.context('./', true, /\.vue$/i)
+files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key)));
 /////////////////////////////////////////////////////////////////////////////
 
 /////////////////////////////////////////////////////////////////////////////
