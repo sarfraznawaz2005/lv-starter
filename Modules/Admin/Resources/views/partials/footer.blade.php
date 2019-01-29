8481ce423920c406ce39393424af498c6898dba6
Sarfraz Ahmed <sarfraz@onsupport.com>
2019-01-29 12:48:29 +0500

    update

--- Modules/Admin/Resources/views/partials/footer.blade.php
+++ Modules/Admin/Resources/views/partials/footer.blade.php
@@ -1,3 +1,8 @@
+{{-- IE es6 pollyfill --}}
+@if(stripos(request()->header('User-Agent'),'Trident') !== false)
+    <script src="/modules/core/js/pollyfills.js"></script>
+@endif
+
 <!-- Scripts -->
 <script src="{{mix('/js/app.js')}}"></script>
 <script src="/modules/admin/js/custom.js"></script>
--- Modules/Main/Resources/views/layouts/partials/footer.blade.php
+++ Modules/Main/Resources/views/layouts/partials/footer.blade.php
@@ -5,6 +5,11 @@
     </div>
 </footer>
 
+{{-- IE es6 pollyfill --}}
+@if(stripos(request()->header('User-Agent'),'Trident') !== false)
+    <script src="/modules/core/js/pollyfills.js"></script>
+@endif
+
 <!-- Scripts -->
 <script src="{{mix('/js/app.js')}}"></script>
 <script src="/modules/main/js/custom.js"></script>
