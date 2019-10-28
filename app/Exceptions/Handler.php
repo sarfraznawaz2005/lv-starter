62b9df465a37152850760f3931f5a82f0dd674cf
Sarfraz Ahmed <sarfraz@onsupport.com>
2019-10-28 10:42:07 +0500

    update

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,6 +6,10 @@ use Exception;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Session\TokenMismatchException;
+use Mail;
+use Symfony\Component\Debug\Exception\FlattenException;
+use Symfony\Component\Debug\ExceptionHandler as SymfonyExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -15,7 +19,9 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        //
+        HttpException::class,
+        ModelNotFoundException::class,
+        TokenMismatchException::class,
     ];
 
     /**
@@ -33,20 +39,65 @@ class Handler extends ExceptionHandler
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Exception  $exception
+     * @param \Exception $exception
      * @return void
      * @throws Exception
      */
     public function report(Exception $exception)
     {
+        if ($this->shouldReport($exception)) {
+            //$this->notifyDevTeam($exception);
+        }
+
         parent::report($exception);
+    }
+
+    /**
+     * Sends an email to the developer about the exception.
+     *
+     * @param \Exception $exception
+     * @return void
+     */
+    protected function notifyDevTeam(Exception $exception)
+    {
+        $ignoredKeywords = [
+            'word1',
+        ];
+
+        if (app()->environment(['production', 'live'])) {
+            try {
+                $sendEmail = true;
+
+                $e = FlattenException::create($exception);
+                $handler = new SymfonyExceptionHandler();
+                $html = $handler->getHtml($e);
+                $message = $e->getMessage();
+
+                foreach ($ignoredKeywords as $keyword) {
+                    if (stripos($message, $keyword) !== false) {
+                        $sendEmail = false;
+                    }
+                }
+
+                if ($sendEmail) {
+                    Mail::send([], [], static function ($message) use ($html) {
+                        $message->to(['dev@email.com', 'ukafeel@onsupport.com'])
+                            ->subject(config('app.name') . ' - ' . 'Error Alert')
+                            ->setBody($html, 'text/html');
+                    });
+                }
+
+            } catch (Exception $ex) {
+                \Log::warning('Could not send error email to dev team!');
+            }
+        }
     }
 
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param \Illuminate\Http\Request $request
+     * @param \Exception $exception
      * @return \Illuminate\Http\Response|string|\Symfony\Component\HttpFoundation\Response
      */
     public function render($request, Exception $exception)
